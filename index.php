bd6c9fab87e4d659253a35c3b64a70a594bdd05b
ripalpiana <rivalviana00@gmail.com>
2024-02-08 21:02:35 +0700

    menambahakn paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem distinctio voluptatibus vero quod accusamus tenetur reprehenderit, ad beatae vel obcaecati id laboriosam temporibus magnam fuga a numquam cupiditate ab fugiat.</p>
 </body>
 </html>
